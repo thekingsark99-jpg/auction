5a1c0b7b3086e4cdc052c0f616586cb26139bd46
thekingsark <kingsley@kingsideaconnections.org>
2025-06-30 18:07:50 -0600

    Add firebase.ts (no longer ignored in .gitignore)

---
+++ biddo_web/src/firebase.ts
@@ -0,0 +1,10 @@
+// Your firebase config and any initialization:
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+}
