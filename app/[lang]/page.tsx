884d3e5c56891cea89bb51a7583f0971817b5853
thekingsark99-jpg <thekingsark99@gmail.com>
2025-08-21 02:49:43 -0600

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,13 +1,7 @@
 import { getSettings } from '../../utils/api';
 
-type PageProps = {
-      params: Promise<{ lang: string }>; // params is now a Promise
-      // searchParams?: Promise<{ [key: string]: string | string[] | undefined }>; // if using searchParams
-    };
-
-    export default async function Page({ params }: PageProps) {
-      const resolvedParams = await params; // Await the promise
-      const { lang } = resolvedParams;
+export default async function Page({ params }: { params: { lang: string } }) {
+  const settings = await getSettings();
   return (
     <main>
       <h1>Language: {params.lang}</h1>
