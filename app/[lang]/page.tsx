9d9c40dccadd55fe223857405f0b83048c9b0c8e
thekingsark99-jpg <thekingsark99@gmail.com>
2025-08-21 02:25:00 -0600

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,11 +2,8 @@ import { getSettings } from '../../utils/api';
 
 import use from 'react' // import this line
 
-type tParams = Promise<{ slug: string[] }>;
-
 export default function Challenge({ params }: { params:  tParams }) {
   const { slug }: {slug: string[]} = use(params) ; // fix this line
-  const productID = slug[1];
   return (
     <main>
       <h1>Language: {params.lang}</h1>
