da42fad8f6f0207f4c7b5e89e347a40908006da5
thekingsark99-jpg <thekingsark99@gmail.com>
2025-08-22 22:19:30 -0600

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,8 +1,8 @@
-type LanguagePageProps = {
+type PageProps = {
   params: { lang: string }
 };
 
-export default function Page({ params }: LanguagePageProps) {
+export default function Page({ params }: PageProps) {
   return (
     <main>
       <h1>Language: {params.lang}</h1>
