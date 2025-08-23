334aaa2ec2d23fcb5b3e0840ad392b26682b7af3
thekingsark99-jpg <thekingsark99@gmail.com>
2025-08-22 18:59:06 -0600

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -12,8 +12,69 @@ export default async function Page({ params }: { params: { lang: string } }) {
 
 export function generateStaticParams() {
   return [
-    { lang: 'en' },
-    { lang: 'fr' },
-    { lang: 'es' }
-  ];
+   en: {
+    name: 'English',
+    icon: 'flags/english1',
+  },
+  es: {
+    name: 'Español',
+    icon: 'flags/spain',
+  },
+  fr: {
+    name: 'Français',
+    icon: 'flags/france',
+  },
+  de: {
+    name: 'Deutsch',
+    icon: 'flags/germany',
+  },
+  it: {
+    name: 'Italiano',
+    icon: 'flags/italy',
+  },
+  ja: {
+    name: '日本語',
+    icon: 'flags/japan',
+  },
+  ro: {
+    name: 'Română',
+    icon: 'flags/romania',
+  },
+  ar: {
+    name: 'العربية',
+    icon: 'flags/arabic',
+  },
+  hi: {
+    name: 'हिंदी',
+    icon: 'flags/india',
+  },
+  pt: {
+    name: 'Português',
+    icon: 'flags/portugal',
+  },
+  ru: {
+    name: 'Русский',
+    icon: 'flags/russia',
+  },
+  tr: {
+    name: 'Türkçe',
+    icon: 'flags/turkey',
+  },
+  zh: {
+    name: '中文',
+    icon: 'flags/china',
+  },
+  hu: {
+    name: 'Magyar',
+    icon: 'flags/hungary',
+  },
+  pl: {
+    name: 'Polski',
+    icon: 'flags/poland',
+  },
+  uk: {
+    name: 'Українська',
+    icon: 'flags/ukraine',
+  },
 }
+
